c7aba18561eed61adc955aa1a72b63ccb04e8e07
DieuLe <108915125+dieu-le-kms@users.noreply.github.com>
2024-04-17 17:44:43 +0700

    Init project

---
+++ playwright.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from "@playwright/test";
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: "./tests",
+  reporter: [
+    ['html'],
+    ['list', {printSteps: true}],
+    ['allure-playwright',
+      {
+        suiteTitle: false,
+        detail: true
+      }
+    ],
+],
+
+  // Time out
+  timeout: 60 * 1000,
+  expect: {
+    timeout: 2 * 60 * 1000,
+  },
+  // fullyParallel: true,
+  workers: 2,
+  use: {
+    actionTimeout: 60 * 1000,
+    navigationTimeout: 3 * 60 * 1000,
+    trace: "off",
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: "chromium",
+    },
+
+    // {
+    //   name: 'firefox',
+    // },
+
+    // {
+    //   name: 'webkit',
+    // },
+  ],
+});
