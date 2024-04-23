85809a8f9eb6c26ca3a920349fdd211c9aef3eae
DieuLe <108915125+dieu-le-kms@users.noreply.github.com>
2024-04-23 12:11:45 +0700

    Update test step

--- tests/home-work.spec.ts
+++ tests/home-work.spec.ts
@@ -1,3 +1,4 @@
+
 import { Locator, expect, test } from '@playwright/test';
 
 test('Verify user can create an order @homework', async ({ page }) => {
@@ -17,42 +18,51 @@ test('Verify user can create an order @homework', async ({ page }) => {
   const orderNumberByProductName = (name: string) => page.locator(`tr[class='ng-star-inserted']`).filter({ hasText: name }).locator(`th[scope]`);
   const yourOrderLabel = page.getByText('Your Orders');
 
-  await page.goto('https://rahulshettyacademy.com/client');
-  await userEmailLocator.fill('rahulshetty@gmail.com');
-  await userPasswordLocator.fill('Iamking@000');
-  await loginButton.click();
-
-  await expect(addToCartButton(productName)).toBeVisible();
-  await myOrdersButton.click();
-  await expect(yourOrderLabel).toBeVisible();
-  await page.waitForTimeout(2000);
-  const deleteButtons = await deleteButton.all();
-  deleteButtons.forEach(async btn => {
-    await btn.click();
-    await page.waitForResponse(resp => resp.url().includes('/delete-order') && resp.request().method() === 'DELETE');
-    await page.waitForResponse(resp => resp.url().includes('/get-orders-for-customer') && resp.request().method() === 'GET');
+  await test.step('Navigate to the website and login', async () => {
+    await page.goto('https://rahulshettyacademy.com/client');
+    await userEmailLocator.fill('rahulshetty@gmail.com');
+    await userPasswordLocator.fill('Iamking@000');
+    await loginButton.click();
+    await expect(addToCartButton(productName)).toBeVisible();
   });
-  await expect(deleteButton.first()).toBeHidden();
-  await dashboardButton.click();
-  await expect(addToCartButton(productName)).toBeVisible();
-  await addToCartButton(productName).click();
 
-  await cartButton.click();
-  await checkoutButton.click();
-  await countryTextbox.fill('Viet');
-  //click backspace to remove 1 character
-  await page.keyboard.press('Backspace');
-  await vietnamOption.click();
-  await submitButton.click();
+  await test.step('Delete all products which still in order list', async () => {
+    await myOrdersButton.click();
+    await expect(yourOrderLabel).toBeVisible();
+    await page.waitForTimeout(2000);
+    const deleteButtons = await deleteButton.all();
+    deleteButtons.forEach(async btn => {
+      await btn.click();
+      await page.waitForResponse(resp => resp.url().includes('/delete-order') && resp.request().method() === 'DELETE');
+      await page.waitForResponse(resp => resp.url().includes('/get-orders-for-customer') && resp.request().method() === 'GET');
+    });
+    await expect(deleteButton.first()).toBeHidden();
+  });
 
-  //wait a little bit then get the order number from the URL
-  await page.waitForTimeout(2000);
-  const currentURL = page.url();
-  const match = currentURL.match(/%5B%22(.*?)%22%5D/);
-  const expectedOrderNumber = match ? match[1] : null;
-  await myOrdersButton.click();
-  await page.waitForResponse(resp => resp.url().includes('/get-orders-for-customer') && resp.request().method() === 'GET');
-  await expect(orderNumberByProductName('IPHONE 13 PRO').first()).toHaveText(expectedOrderNumber!);
+  await test.step('Add a product to cart and checkout', async () => {
+    await dashboardButton.click();
+    await expect(addToCartButton(productName)).toBeVisible();
+    await addToCartButton(productName).click();
+
+    await cartButton.click();
+    await checkoutButton.click();
+    await countryTextbox.fill('Viet');
+    //click backspace to remove 1 character
+    await page.keyboard.press('Backspace');
+    await vietnamOption.click();
+    await submitButton.click();
+  });
+
+  await test.step('Verify the order number', async () => {
+    //wait a little bit then get the order number from the URL
+    await page.waitForTimeout(2000);
+    const currentURL = page.url();
+    const match = currentURL.match(/%5B%22(.*?)%22%5D/);
+    const expectedOrderNumber = match ? match[1] : null;
+    await myOrdersButton.click();
+    await page.waitForResponse(resp => resp.url().includes('/get-orders-for-customer') && resp.request().method() === 'GET');
+    await expect(orderNumberByProductName('IPHONE 13 PRO').first()).toHaveText(expectedOrderNumber!);
+  });
 });
 
 test.describe('Login feature tests', () => {
